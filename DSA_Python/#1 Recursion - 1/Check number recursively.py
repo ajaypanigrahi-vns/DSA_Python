34e8d7c1cc7fbfe01f2323fbaa8fe2387500751e
Ajay <ajay261095@gmail.com>
2024-08-16 23:38:39 +0530

    checkNumber

---
+++ DSA_Python/#1 Recursion - 1/Check number recursively.py
@@ -0,0 +1,16 @@
+def checkNumber(arr, x):
+    for num in arr:
+        if num ==x:
+            return True
+    return False
+
+# Main
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+n=int(input())
+arr=list(int(i) for i in input().strip().split(' '))
+x=int(input())
+if checkNumber(arr, x):
+    print('true')
+else:
+    print('false')
